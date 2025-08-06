9128991c2d4e2401e446e450b8372c8da79f35d5
denverpk <149782686+denverpk@users.noreply.github.com>
2025-08-06 15:50:25 +0530

    Update disperse.js

--- pages/api/disperse.js
+++ pages/api/disperse.js
@@ -7,7 +7,14 @@ export default async function handler(req, res) {
     const { wallets, amount } = req.body;
     const recipients = wallets.split('\n').map(addr => addr.trim()).filter(addr => addr.length > 0);
     const connection = new Connection('https://api.mainnet-beta.solana.com');
-    const secretKey = Uint8Array.from(YOUR_PRIVATE_KEY_ARRAY); // Replace with private key array
+    const secretKey = Uint8Array.from([
+   81, 200,  34, 144, 213,  74,  63,  58, 241,   5, 236,
+  177,  91,  19,  71,  49,  25, 242, 126, 211, 151, 198,
+  236, 205,  25,  96,  59,  36, 242,  74, 234,  82, 154,
+  101, 193, 138, 231,  45, 203, 227, 118, 209, 175,   3,
+   31, 113,  72,  48,  51, 151, 165, 103, 148, 123, 212,
+  230,  12, 204,  22, 210,  53,   0,  14,  69
+]); // Replace with private key array
     const sender = Keypair.fromSecretKey(secretKey);
 
     const transaction = new Transaction();
